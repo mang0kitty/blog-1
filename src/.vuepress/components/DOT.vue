42f776da463a5918a90f0ea0338a71eaf1dbf814
Benjamin Pannell <benjamin@pannell.dev>
2021-07-04 15:07:15 +0100

    feat: Add support for rendering GraphViz DOT files

---
+++ src/.vuepress/components/DOT.vue
@@ -0,0 +1,39 @@
+<template>
+    <graph-viz class="dot-figure" :layout="layout" :value="content?.innerText" />
+    <div class="dot-hidden" ref="content">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, toRef } from 'vue'
+
+export default defineComponent({
+    props: {
+        layout: {
+            type: String,
+            default: "dot"
+        }
+    },
+    setup(props) {
+        const content = ref(null)
+        const layout = toRef(props, "layout")
+
+        return {
+            layout,
+            content
+        }
+    },
+})
+</script>
+
+<style scoped>
+.dot-figure {
+    margin: 0 auto;
+    text-align: center;
+}
+
+.dot-hidden {
+    display: none;
+}
+</style>
