762f3fdc084191c833834bd88065079d044e7d3e
Benjamin Pannell <benjamin@pannell.dev>
2022-09-07 22:15:43 +0100

    fix: Restore layouts

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,4 +1,7 @@
 import {defineClientConfig} from '@vuepress/client'
+import Layout from "./layouts/Layout.vue"
+import BlogPost from "./layouts/BlogPost.vue"
+import GitHubProject from "./layouts/GitHubProject.vue"
 
 export default defineClientConfig({
     layouts: {
